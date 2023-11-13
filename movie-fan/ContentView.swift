49ae56e0b2eea9ce18d70da9771ab392fba58fc4
igorfgama <igor.franca@discente.univasf.edu.br>
2023-11-13 00:26:19 -0300

    Initial Commit

---
+++ movie-fan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-fan
+//
+//  Created by Igor Franca Gama on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-fan/movie_fanApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_fanApp.swift
+//  movie-fan
+//
+//  Created by Igor Franca Gama on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct movie_fanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
